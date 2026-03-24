27f55077219e7b5b3b933a74da263763166e3d50
dan3ort35 <dan3ort@gmail.com>
2026-03-25 01:43:30 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by dan3ort on 3/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by dan3ort on 3/25/26.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
