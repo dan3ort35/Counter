b2aeb5163c592653d6de1c61bcd355180f772370
dan3ort35 <dan3ort@gmail.com>
2026-03-25 02:32:47 +0300

    update full repasitory sprint_3
    не было всего проекта в репазитории

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,19 +1,29 @@
-//
-//  ViewController.swift
-//  Counter
-//
-//  Created by dan3ort on 3/25/26.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelStep: UILabel!
+    @IBOutlet weak var buttonUp: UIButton!
+    @IBOutlet weak var buttonDown: UIButton!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLabel()
     }
-
-
+    
+    @IBAction func buttonUpAction(_ sender: Any) {
+        counter += 1
+        updateLabel()
+    }
+    
+    @IBAction func buttonDownAction(_ sender: Any) {
+        counter -= 1
+        updateLabel()
+    }
+    
+    private func updateLabel() {
+        labelStep.text = "\(counter)"
+    }
 }
-
